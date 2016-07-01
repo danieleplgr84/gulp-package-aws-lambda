ac29100c875b7b82a1d717b167378c40f2a9f253
daniele <daniele.p@ulixe.com>
2016-07-01 12:32:38 +0200

    first impl;

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
-# gulp-npm-copy-deps
+# gulp-package-aws-lambda
 
-Gulp plugin for copying package.json dependencies recursively into a target dir
+Gulp plugin 
 
 ## Get started
 
 ### Install
 
 ```
-npm install gulp-npm-copy-deps
+npm gulp-package-aws-lambda
 ```
 
 ### Examples
 
 ```javascript
 var gulp = require('gulp');
-var gncd = require('gulp-npm-copy-deps');
 
-// Copy dependencies to build/node_modules/
-gulp.task('copy-npm-deps', function() {
-	return gncd('./', './build');
-});  
+
 ```
 
--- src/index.js
+++ src/index.js
@@ -1,68 +1,56 @@
 var fs = require('fs'); 
 var gulp = require('gulp');
-
-//gulp.task('copy-npm-deps', function() {
-//	return gulp.src(listDeps(), {base:'./'}).pipe(gulp.dest('./build'));
-//});
+var jslint = require('gulp-jslint');
+var gncd = require('gulp-npm-copy-deps');
 
 
-var _add_dependency_to_list = function(_list, _dep_string){
-	if(_list && _dep_string){
-		var to_add_path_src = './node_modules/' + _dep_string + '/**/*'
-		if(_list.indexOf(to_add_path_src) == -1){ 
-			_list.push(to_add_path_src);
-		}
-	} 
-};
+module.exports = function(){
 
+  var package_json = require('./package.json'); 
+  console.log(">> package_json " + JSON.stringify(package_json));
 
-var _pushDependenciesForModuleRecursively =function(_list, isRootModule, module_name){
-	var package_json = null; 
-	//console.log(">> Calling _pushDependenciesForModuleRecursively for "+module_name);
-	
-	if(isRootModule){
-		var buffer = fs.readFileSync('./package.json');
-		package_json = JSON.parse(buffer.toString());  
-		//console.log(">> package_json >> " + JSON.stringify(package_json.dependencies));
-	} 
-	else {
-		var buffer = fs.readFileSync('./node_modules/'+module_name+'/package.json');
-		var child_package_json = JSON.parse(buffer.toString());   
-		package_json = child_package_json;
-		//console.log(">> child_package_json >> " + JSON.stringify(package_json.dependencies));
-	}
-	
-	var dependencies_list = Object.keys(package_json.dependencies);
-	//console.log(">> Found dependencies for module "+module_name+" >> " + JSON.stringify(dependencies_list ));
-	
-	if(dependencies_list && dependencies_list.length >0){ 
-		for (index in dependencies_list) {
-			var _dependency_mod_name = dependencies_list[index];
-			if(_dependency_mod_name){
-				_pushDependenciesForModuleRecursively(_list, false, _dependency_mod_name); 
-				_add_dependency_to_list(_list, _dependency_mod_name); 
-			} 
-		} 
-	}
-	else {
-		if(!isRootModule && module_name && typeof(module_name)==='string'){
-			var to_add_path_src = './node_modules/' + module_name + '/**/*'
-			_add_dependency_to_list(_list, module_name); 
-		} 
-	}
-};
-
-
-var listDeps = function(devDependencies, packageJsonFilePath) {  
-  var keys = [];
-  var isRootModule = true;
   
-  _pushDependenciesForModuleRecursively(keys, isRootModule); 
   
-  return keys;
+  gulp.task('pal-clean-dist', function () {
+  	return del(['dist/**/*', './dist']);
+  }); 
+  
+  gulp.task('pal-jshint', ['pal-clean-dist'], function () { 
+	  return gulp.src(['*.js', 'src/**/*.js']).pipe(jshint());
+  });
+  
+  
+
+  gulp.task('pal-copy-npm-deps-into-tmp', ['clean-dist', 'pal-jshint'], function() {
+  	return gncd('./', './dist/tmp');
+  }); 
+
+  gulp.task('pal-copy-src-files-into-tmp', ['clean-dist', 'pal-jshint'], function() {
+  	var src_files = package_json.main;
+  	return gulp.src(src_files, {base: "./src"}) 
+  		.pipe(gulp.dest('./dist/tmp'));
+  });
+  
+  
+  //Zip Task
+  gulp.task('pal-zip-files', ['clean-dist', 'pal-jshint', 'copy-src-files-into-tmp', 'copy-npm-deps-into-tmp'], function() {
+  	// copy the src files
+  	var src_files = package_json.main; 
+  	var zip_filename = 'index.zip';
+  	
+  	var _task = gulp.src(['*'], {base: "./dist/tmp"})
+  		.pipe(zip(zip_filename))
+  		.pipe(gulp.dest('dist'));
+  	
+  	return _task;
+  });
+
+
+  // Export Task
+  gulp.task('package-aws-lambda', ['clean-dist', 'pal-jshint', 'copy-src-files-into-tmp', 'copy-npm-deps-into-tmp', 'pal-zip-files']);
+  
+  
 };
 
 
-module.exports = function(_base, _dest){
-	return gulp.src(listDeps(), {base: _base}).pipe(gulp.dest(_dest));
-};
+
